3202416ae5dd4e67876a1d37c9e850a388fb3f6c
Lcerous <lcerous.a.diemust@gmail.com>
2024-04-25 13:39:07 +0700

    enemy and projectile

--- Assets/Script/Code(L)/Enemy.cs
+++ Assets/Script/Code(L)/Enemy.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingBoat : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +14,14 @@ public class MovingBoat : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Script/Code(L)/GameManager.cs
+++ Assets/Script/Code(L)/GameManager.cs
@@ -2,24 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CanonController : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    Rigidbody2D rb;
-    
+    [SerializeField] private GameObject[] enemyShip;
+
+    void Awake()
+    {
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Control();
+        
     }
 
-    private void Control()
+    private void YouWin()
     {
-        
+        if (enemyShip.Length <= 0)
+        {
+
+        }
     }
 }
